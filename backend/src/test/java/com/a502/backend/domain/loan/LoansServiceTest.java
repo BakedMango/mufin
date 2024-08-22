1f44b3b55092c003f96e7eff5bad262f7bdd19c3
siyoon <ryuu.public@gmail.com>
2024-08-23 01:19:49 +0900

    test: business layer of Loan

--- backend/src/test/java/com/a502/backend/domain/loan/LoansServiceTest.java
+++ backend/src/test/java/com/a502/backend/domain/loan/LoansServiceTest.java
@@ -1,24 +1,23 @@
 package com.a502.backend.domain.loan;
 
+import com.a502.backend.application.entity.Code;
 import com.a502.backend.application.entity.Loan;
 import com.a502.backend.application.entity.User;
+import com.a502.backend.domain.code.CodeFactory;
 import com.a502.backend.domain.user.UserFactory;
+import com.a502.backend.domain.user.UserRepository;
+import com.a502.backend.global.code.CodeRepository;
 import com.a502.backend.global.error.BusinessException;
 import com.a502.backend.global.exception.ErrorCode;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class LoansServiceTest {
@@ -31,8 +30,23 @@ class LoansServiceTest {
 
     @Autowired
     UserFactory userFactory;
+
+    @Autowired
+    CodeFactory codeFactory;
+
     @Autowired
     private LoansRepository loansRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private CodeRepository codeRepository;
+
+    @AfterEach
+    void tearDown() {
+        loansRepository.deleteAllInBatch();
+        userRepository.deleteAllInBatch();
+        codeRepository.deleteAllInBatch();
+    }
 
     @Test
     void testFindById_Exists() {
@@ -93,12 +107,21 @@ class LoansServiceTest {
         assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.API_ERROR_LOAN_NOT_EXIST);
     }
 
-    @Disabled("Code 엔티티에 대해 모름")
     @Test
     void testGetAllLoansForChild_WithLoans() {
         // given
-        Loan savedLoan = loanFactory.createAndSaveLoan(1000, 30, 10, "kim");
-        User child = savedLoan.getChild();
+        Code judgedCode = codeFactory.createAndSaveCode("L001", "심사중");
+        Code inProgressCode = codeFactory.createAndSaveCode("L002", "진행");
+        Code refusedCode = codeFactory.createAndSaveCode("L003", "거절");
+        Code repaidCode = codeFactory.createAndSaveCode("L004", "상환완료");
+        User child = userFactory.createAndSaveUser("kim");
+        int n = 3;
+        for (int i = 0; i < n; i++) {
+            loanFactory.createAndSaveLoanWithChild(1000, 30, 10, child, refusedCode);
+            loanFactory.createAndSaveLoanWithChild(1000, 30, 10, child, judgedCode);
+            loanFactory.createAndSaveLoanWithChild(1000, 30, 10, child, inProgressCode);
+        }
+        loanFactory.createAndSaveLoanWithChild(1000, 30, 10, child, repaidCode);
 
         // when
         List<Loan> loans = loansService.getAllLoansForChild(child);
@@ -106,14 +129,21 @@ class LoansServiceTest {
         // then
         assertThat(loans)
                 .isNotEmpty()
-                .hasSize(1);
+                .hasSize(n * 3);
     }
 
-    @Disabled("Code 엔티티에 대해 모름")
     @Test
     void testGetAllLoansForChild_NoLoans() {
         // given
+        Code judgedCode = codeFactory.createAndSaveCode("L001", "심사중");
+        Code inProgressCode = codeFactory.createAndSaveCode("L002", "진행");
+        Code refusedCode = codeFactory.createAndSaveCode("L003", "거절");
+        Code repaidCode = codeFactory.createAndSaveCode("L004", "상환완료");
         User child = userFactory.createAndSaveUser("kim");
+
+        loanFactory.createAndSaveLoan(1000, 30, 10, "kim", judgedCode);
+        loanFactory.createAndSaveLoan(1000, 30, 10, "kim", refusedCode);
+        loanFactory.createAndSaveLoan(1000, 30, 10, "kim", repaidCode);
 
         // when&then
         BusinessException exception = assertThrows(BusinessException.class, () -> loansService.getAllLoansForChild(child));
@@ -131,5 +161,73 @@ class LoansServiceTest {
 
         // then
         assertThat(loan.getOverdueCnt()).isEqualTo(overdueCount + 1);
+    }
+
+    @Test
+    void testGetAllLoansForParents_WithLoans() {
+        // given
+        Code judgedCode = codeFactory.createAndSaveCode("L001", "심사중");
+        Code inProgressCode = codeFactory.createAndSaveCode("L002", "진행");
+        Code refusedCode = codeFactory.createAndSaveCode("L003", "거절");
+        Code repaidCode = codeFactory.createAndSaveCode("L004", "상환완료");
+        User parents = userFactory.createAndSaveUser("kim");
+        int n = 3;
+        for (int i = 0; i < n; i++) {
+            loanFactory.createAndSaveLoanWithParent(1000, 30, 10, parents, inProgressCode);
+            loanFactory.createAndSaveLoanWithParent(1000, 30, 10, parents, repaidCode);
+        }
+        loanFactory.createAndSaveLoanWithParent(1000, 30, 10, parents, refusedCode);
+        loanFactory.createAndSaveLoanWithParent(1000, 30, 10, parents, judgedCode);
+
+        // when
+        List<Loan> loans = loansService.getAllLoansForParents(parents);
+
+        // then
+        assertThat(loans)
+                .isNotEmpty()
+                .hasSize(n * 2);
+    }
+
+    @Test
+    void testGetAllLoansForParents_NoLoans() {
+        // given
+        Code judgedCode = codeFactory.createAndSaveCode("L001", "심사중");
+        Code inProgressCode = codeFactory.createAndSaveCode("L002", "진행");
+        Code refusedCode = codeFactory.createAndSaveCode("L003", "거절");
+        Code repaidCode = codeFactory.createAndSaveCode("L004", "상환완료");
+        User parents = userFactory.createAndSaveUser("kim");
+        User otherParents = userFactory.createAndSaveUser("lee");
+
+        loanFactory.createAndSaveLoanWithParent(1000, 30, 10, otherParents, judgedCode);
+        loanFactory.createAndSaveLoanWithParent(1000, 30, 10, otherParents, refusedCode);
+        loanFactory.createAndSaveLoanWithParent(1000, 30, 10, otherParents, repaidCode);
+
+        // when&then
+        BusinessException exception = assertThrows(BusinessException.class, () -> loansService.getAllLoansForParents(parents));
+        assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.API_ERROR_LOAN_NOT_EXIST_FOR_PARENTS);
+    }
+
+    @Test
+    void findAllLoansInProgress() {
+        // given
+        Code judgedCode = codeFactory.createAndSaveCode("L001", "심사중");
+        Code inProgressCode = codeFactory.createAndSaveCode("L002", "진행");
+        Code refusedCode = codeFactory.createAndSaveCode("L003", "거절");
+        Code repaidCode = codeFactory.createAndSaveCode("L004", "상환완료");
+        int n = 3;
+        for (int i = 0; i < n; i++) {
+            loanFactory.createAndSaveLoan(1000, 30, 10, "kim", inProgressCode);
+        }
+        loanFactory.createAndSaveLoan(1000, 30, 10, "kim", repaidCode);
+        loanFactory.createAndSaveLoan(1000, 30, 10, "kim", refusedCode);
+        loanFactory.createAndSaveLoan(1000, 30, 10, "kim", judgedCode);
+
+        // when
+        List<Loan> loans = loansService.findAllLoansInProgress();
+
+        // then
+        assertThat(loans)
+                .isNotEmpty()
+                .hasSize(n);
     }
 }
