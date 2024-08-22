ed46ad3e30e5e5df47d6038878c55c448f6feaa7
siyoon <ryuu.public@gmail.com>
2024-08-23 01:19:19 +0900

    feat: add util methods for Loan factory class

--- backend/src/test/java/com/a502/backend/domain/loan/LoanFactory.java
+++ backend/src/test/java/com/a502/backend/domain/loan/LoanFactory.java
@@ -1,5 +1,6 @@
 package com.a502.backend.domain.loan;
 
+import com.a502.backend.application.entity.Code;
 import com.a502.backend.application.entity.Loan;
 import com.a502.backend.application.entity.User;
 import com.a502.backend.domain.user.UserFactory;
@@ -28,6 +29,55 @@ public class LoanFactory {
                 .paymentTotalCnt(count)
                 .startDate(LocalDate.now())
                 .child(user)
+                .build();
+
+        return loansRepository.save(loan);
+    }
+
+    public Loan createAndSaveLoanWithChild(int amount, int period, int count, User child, Code code) {
+
+        Loan loan = Loan.builder()
+                .amount(amount)
+                .reason("Test Reason")
+                .paymentDate(period)
+                .penalty("None")
+                .paymentTotalCnt(count)
+                .startDate(LocalDate.now())
+                .child(child)
+                .code(code)
+                .build();
+
+        return loansRepository.save(loan);
+    }
+
+    public Loan createAndSaveLoanWithParent(int amount, int period, int count, User parent, Code code) {
+
+        Loan loan = Loan.builder()
+                .amount(amount)
+                .reason("Test Reason")
+                .paymentDate(period)
+                .penalty("None")
+                .paymentTotalCnt(count)
+                .startDate(LocalDate.now())
+                .parent(parent)
+                .code(code)
+                .build();
+
+        return loansRepository.save(loan);
+    }
+
+    public Loan createAndSaveLoan(int amount, int period, int count, String childName, Code code) {
+
+        User user = userFactory.createAndSaveUser(childName);
+        Loan loan = Loan.builder()
+                .amount(amount)
+                .reason("Test Reason")
+                .paymentDate(period)
+                .penalty("None")
+                .paymentTotalCnt(count)
+                .startDate(LocalDate.now())
+                .child(user)
+                .code(code)
                 .build();
 
         return loansRepository.save(loan);
